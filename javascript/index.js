2df0f9de68556099dc09e4521378120e9925ae5b
Jackson Silva <jackson.silva.who@gmail.com>
2020-09-05 19:18:42 -0300

    Atualizando métodos e adicionando link de mais uma imagem.

--- javascript/index.js
+++ javascript/index.js
@@ -6,16 +6,16 @@ let answers = ['alert()']
 let i = 0;
 
 let screen = {
-    show: function (t) {
-        t.classList.remove("hide");
-        t.classList.add("show");
+    show: function (item) {
+        item.classList.remove("hide");
+        item.classList.add("show");
     },
-    hide: function (t) {
-        t.classList.remove("show");
-        t.classList.add("hide");
+    hide: function (item) {
+        item.classList.remove("show");
+        item.classList.add("hide");
     },
-    changeCardImage: function (t) {
-        t.src = '';
+    changeCardImage: function (item, img) {
+        item.src = img;
     },
     changeText: function (item, message) {
         item.innerHTML = message;
@@ -55,13 +55,15 @@ function changeOptions() {
     let message2 = document.querySelector("#message2");
     let cardTitle = document.querySelector("#card-title");
     let cardText = document.querySelector("#card-text");
+    let cardImage = document.querySelector('#card-image');
     let btn1 = document.querySelector("#btn1");
     let btn2 = document.querySelector("#btn2");
 
     setTimeout(screen.changeText, 2000, message1, 'Decisão liberada!');
+    setTimeout(screen.changeCardImage, 2000, cardImage, 'https://i.imgur.com/jYHn5fl.png');
     setTimeout(screen.changeText, 2000, cardTitle, 'Yamato conseguiu hackear!');
     setTimeout(screen.changeText, 2000, message2, 'Selecione o que quer fazer:');
-    setTimeout(screen.changeText, 2000, cardText, 'O governo dos EUA viu potêncial e te enviou uma mensagem oferecendo uma vaga de emprego.' +
+    setTimeout(screen.changeText, 2000, cardText, 'O governo dos EUA viu potêncial e espalhou nos noticiarios que o Yamato pode trabalhar p/ eles.' +
         ' Caso rejeite, entrará pra lista dos mais procurados do mundo!');
     setTimeout(screen.changeButtonValue, 2000, btn1, 'Aceitar emprego');
     setTimeout(screen.changeButtonValue, 2000, btn2, 'Rejeitar emprego');
